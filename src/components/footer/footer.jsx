a89eeb64af2d7f7eeae9ccbda8f578a3e792f841
Kamalov <71275715+KamolovFarrux2005@users.noreply.github.com>
2023-11-23 00:32:10 +0500

    Update footer.jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="mt-[50px] lg:mt-[150px] py-[35px]">
             <Wrapper>
                 <div className="flex items-start justify-between flex-col lg:flex-row gap-[15px] lg:gap-0">
-                    <img src={logo} alt="logo" className="h-[115px] w-[180px] object-cover" />
+                    <img src={logo} alt="logo" className="h-[115px] w-[380px] object-cover" />
                     <div>
                         <p className="text-lg text-white">Aloqaga chiqing</p>
                         <div className="mt-[8px] flex flex-col gap-1">
